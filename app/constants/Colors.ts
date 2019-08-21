6a77b6d69fce9e11cca99a3e71d084b505e95c1c
Karan Pratap Singh <synester.gates4@gmail.com>
2019-08-21 20:28:04 +0530

    added constants

---
+++ app/constants/Colors.ts
@@ -0,0 +1,19 @@
+/**
+ * Colors:
+ * This contains all the color config for the application
+ */
+
+const Colors = {
+  primary: {
+    regular: '#C7A17A',
+    dark: '#3A2311',
+    light: '#EAE7DE',
+  },
+  secondary: {
+    regular: '#F4F4F4',
+    dark: '#74736E',
+    light: '#FFFFFF',
+  },
+};
+
+export default Colors;
---
+++ app/constants/Typography.ts
@@ -0,0 +1,30 @@
+/**
+ * Typography:
+ * This contains all the typography config for the application
+ * #Note: color and font size are defaulted as they can be overridden
+ *        as required.
+ */
+const Typography = {
+  heading: {
+    fontSize: 28,
+    fontFamily: 'AirbnbCereal-Bold',
+    color: '#000',
+  },
+  subHeading: {
+    fontSize: 20,
+    fontFamily: 'AirbnbCereal-Medium',
+    color: '#000',
+  },
+  body: {
+    fontSize: 16,
+    fontFamily: 'AirbnbCereal-Light',
+    color: '#000',
+  },
+  caption: {
+    fontSize: 12,
+    fontFamily: 'AirbnbCereal-Light',
+    color: '#000',
+  },
+};
+
+export default Typography;
