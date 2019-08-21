e2e1078ffc4162c44cf588221024b3efc9647549
Karan Pratap Singh <synester.gates4@gmail.com>
2019-08-21 20:41:55 +0530

    added custom fonts, font package

--- App.tsx
+++ App.tsx
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { View } from 'react-native';
 import AppContainer from './app/navigation/App.Navigation';
+import * as Font from 'expo-font'
 
 export default function App() {
+
+  const [isFontLoaded, setFontLoaded] = useState(false);
+
+  const LoadFonts = async () => {
+    await Font.loadAsync({
+      'AirbnbCereal-Bold': require('./assets/fonts/AirbnbCereal-Bold.ttf'),
+      'AirbnbCereal-Medium': require('./assets/fonts/AirbnbCereal-Medium.ttf'),
+      'AirbnbCereal-Light': require('./assets/fonts/AirbnbCereal-Light.ttf')
+    });
+
+    setFontLoaded(true);
+  };
+
+  useEffect(() => {
+    LoadFonts();
+  }, []);
+
+  if (!isFontLoaded) return <View style={{ flex: 1 }} />
   return <AppContainer />
 };
--- app/screen/StationListScreen.tsx
+++ app/screen/StationListScreen.tsx
@@ -1,18 +1,60 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { SectionGrid } from 'react-native-super-grid';
+import { MetroLineSectionListData } from '../constants/FormattedData';
+import Colors from '../constants/Colors';
+import { responsiveWidth } from 'react-native-responsive-dimensions';
+
+console.log(MetroLineSectionListData);
 
 const StationListScreen = props => (
-    <View style={styles.container}>
-        <Text>StationListScreen</Text>
-    </View>
+    <SectionGrid
+        itemDimension={responsiveWidth(100)}
+        sections={MetroLineSectionListData}
+        style={styles.gridView}
+        renderItem={({ item, section, index }) => (
+            <View style={styles.itemContainer}>
+                <Text style={styles.itemName}>{item['25'] || item['Hindi']}</Text>
+            </View>
+        )}
+        renderSectionHeader={({ section }) => (
+            <Text style={styles.sectionHeader}>{section.title}</Text>
+        )}
+    />
 );
 
 const styles = StyleSheet.create({
-    container: {
+    gridView: {
         flex: 1,
+        marginTop: 20,
+    },
+    itemContainer: {
+        // justifyContent: 'flex-end',
+        borderRadius: 5,
+        padding: 10,
+        // height: 150,
+        width: responsiveWidth(94),
+        backgroundColor: Colors.secondary.regular
+    },
+    itemName: {
+        fontSize: 12,
+        color: Colors.primary.dark,
+        fontWeight: '600',
+    },
+    itemCode: {
+        fontWeight: '600',
+        fontSize: 12,
+        color: '#fff'
+    },
+    sectionHeader: {
+        flex: 1,
+        fontSize: 24,
+        fontWeight: '600',
         alignItems: 'center',
-        justifyContent: 'center'
-    }
+        color: '#636e72',
+        backgroundColor: 'white',
+        padding: 10,
+    },
 });
 
 export default StationListScreen;
