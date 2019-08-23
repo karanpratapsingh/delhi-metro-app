7e91235e7fd3be1719ad2a12858f041a8b4cc840
Karan Pratap Singh <synester.gates4@gmail.com>
2019-08-24 01:59:08 +0530

    added new list design with gradient

--- app/screen/StationListScreen.tsx
+++ app/screen/StationListScreen.tsx
@@ -1,41 +1,47 @@
+import { Entypo } from '@expo/vector-icons';
+import { BlurView } from 'expo-blur';
+import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { FlatGrid } from 'react-native-super-grid';
-import { MetroLineListData } from '../constants/FormattedData';
-import Colors from '../constants/Colors';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { responsiveWidth } from 'react-native-responsive-dimensions';
+import { FlatGrid } from 'react-native-super-grid';
+import { useNavigation } from 'react-navigation-hooks';
+import Colors from '../constants/Colors';
+import { MetroLineListData } from '../constants/FormattedData';
 import Typography from '../constants/Typography';
 
-import { LinearGradient } from 'expo-linear-gradient';
-import { BlurView } from 'expo-blur';
+const StationListScreen = () => {
 
-// console.log(MetroLineListData);
+    let { navigate } = useNavigation();
 
-const StationListScreen = props => (
-    <>
-        <View style={{ backgroundColor: 'pink'}}>
-            <Text style={{ ...Typography.heading, }}>Metro Lines</Text>
-        </View>
-        <FlatGrid
-            itemDimension={responsiveWidth(100)}
-            items={MetroLineListData}
-            style={styles.gridView}
-            renderItem={({ item }) => (
-                <LinearGradient style={styles.itemContainer} start={[0, 1]} end={[1, 0]} colors={item.colors}>
-                    <BlurView style={{ flex: 1, padding: 10, paddingVertical: 20 }} tint='dark' intensity={10}>
-                        <Text style={styles.itemName}>{item.title}</Text>
-                    </BlurView>
-
-                </LinearGradient>
-            )}
-        />
-    </>
-);
+    return (
+        <>
+            <View style={{ paddingVertical: 10, paddingHorizontal: 20 }}>
+                <Text style={{ ...Typography.heading, }}>Metro Lines</Text>
+            </View>
+            <FlatGrid
+                showsVerticalScrollIndicator={false}
+                itemDimension={responsiveWidth(100)}
+                items={MetroLineListData}
+                style={styles.gridView}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => navigate('LineInfoScreen', { lineInfo: item })}>
+                        <LinearGradient style={styles.itemContainer} start={[0, 1]} end={[1, 0]} colors={item.colors}>
+                            <BlurView style={{ flex: 1, alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between', padding: 10, paddingVertical: 20 }} tint='dark' intensity={10}>
+                                <Text style={styles.itemName}>{item.title}</Text>
+                                <Entypo name={'chevron-thin-right'} size={25} color={Colors.secondary.light} />
+                            </BlurView>
+                        </LinearGradient>
+                    </TouchableOpacity>
+                )}
+            />
+        </>
+    );
+};
 
 const styles = StyleSheet.create({
     gridView: {
-        flex: 1,
-        marginTop: 20
+        flex: 1
     },
     itemContainer: {
         borderRadius: 5,
