0a4d84a9d0a8cfad9bc57dd8715dd585c210d78f
Karan Pratap Singh <synester.gates4@gmail.com>
2019-08-25 00:10:38 +0530

    added animations

--- app/screen/RouteFinderScreen.tsx
+++ app/screen/RouteFinderScreen.tsx
@@ -1,11 +1,27 @@
+import LottieView from 'lottie-react-native';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-const RouteFinderScreen = props => (
-    <View style={styles.container}>
-        <Text>RouteFinderScreen</Text>
-    </View>
-);
+console.disableYellowBox = true;
+const RouteFinderScreen = () => {
+
+    return (
+
+        <View style={styles.container}>
+            <LottieView
+                ref={animation => {
+                    this.animation = animation;
+                }}
+                autoPlay
+                style={{
+                    width: 400,
+                    height: 400,
+                }}
+                source={require('../../assets/animations/train.json')}
+            />
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
