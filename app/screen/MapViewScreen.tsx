c98beeb2ec84a92d07aef9379e67e80d57fcf50f
Karan Pratap Singh <synester.gates4@gmail.com>
2019-08-25 15:21:21 +0530

    added map screen component

--- app/screen/MapViewScreen.tsx
+++ app/screen/MapViewScreen.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import ReactNativeZoomableView from '@dudigital/react-native-zoomable-view/src/ReactNativeZoomableView';
 
-const MapViewScreen = props => (
+const MapViewScreen = () => (
     <View style={styles.container}>
-        <Text>MapViewScreen</Text>
+        <ReactNativeZoomableView
+            maxZoom={5.0}
+            minZoom={0.5}
+            zoomStep={0.5}
+            initialZoom={1}
+            bindToBorders={true}
+            // onZoomAfter={this.logOutZoomState}
+            style={{}}>
+
+            <Image style={{ flex: 1, width: null, height: '100%' }}
+                source={require('../static/metro-map/metro-map.jpg')}
+                resizeMode='contain' />
+        </ReactNativeZoomableView>
     </View>
 );
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+        // alignItems: 'center',
+        // justifyContent: 'center'
     }
 });
 
